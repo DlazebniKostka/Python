3a11c471c241bc4cd455dd6dbea085f03d0ac984
DlazebniKostka <adam.riha@infis.cz>
2025-03-18 10:59:55 +0100

    Update F-string_ukol1.py

--- F-string_ukol1.py
+++ F-string_ukol1.py
@@ -1,6 +1,6 @@
-print("napiš svoje jméno a věk")
+print("Napiš svoje jméno a věk.")
 
 jmeno = input()
 vek = input()
 
-print (f"Ahoj, jmenuji se {jmeno} a je mi {vek}")
+print (f"Ahoj, jmenuji se {jmeno} a je mi {vek}.")
