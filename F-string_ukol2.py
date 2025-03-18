9907776de3b9018753d4137861229c1b15af1cea
DlazebniKostka <adam.riha@infis.cz>
2025-03-18 11:33:21 +0100

    Add files via upload

---
+++ F-string_ukol2.py
@@ -0,0 +1,12 @@
+print ("Napiš dvě desetinná čísla.")
+
+cislo1 = float (input())
+cislo2 = float (input())
+
+print (f"{cislo1+cislo2:.2f}")
+
+print (f"{cislo1-cislo2:.2f}")
+
+print (f"{cislo1*cislo2:.2f}")
+
+print (f"{cislo1/cislo2:.2f}")
